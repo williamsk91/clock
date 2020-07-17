a7212b6f120e6ecb60e204c0068e4fbdb2528340
williamsk91 <williamskusnandi@gmail.com>
2020-07-18 09:24:00 +1000

    feat: DatePicker with time constraints

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -2,12 +2,13 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 
 import React from "react";
-import {
-  DateRange,
-  DateRangePicker,
-  TimePrecision
-} from "@blueprintjs/datetime";
+import { CloseOutlined } from "@ant-design/icons";
+import { DateInput, DateRange, TimePrecision } from "@blueprintjs/datetime";
+import { Button } from "antd";
+import { addHours, format, isAfter, isBefore } from "date-fns";
 import styled from "styled-components";
+
+import { Spacer } from "./Spacer";
 
 interface Props {
   value: DateRange;
@@ -28,32 +29,79 @@ interface Props {
 export const DatePicker = (props: Props) => {
   const { value, onChange, includeTime } = props;
 
+  const start = value[0];
+  const end = value[1];
+
   return (
-    <StyledDateRangePicker
-      value={value}
-      onChange={onChange}
-      timePrecision={includeTime ? TimePrecision.MINUTE : undefined}
-      shortcuts={false}
-      allowSingleDayRange
-      singleMonthOnly
-      dayPickerProps={{
-        firstDayOfWeek: 1
-      }}
-    />
+    <div>
+      <SingleDatePicker
+        value={start}
+        onChange={(d) => {
+          if (!d) return onChange([null, end]);
+
+          const newStart = end && isBefore(end, d) ? addHours(end, -1) : d;
+          onChange([newStart, end]);
+        }}
+        maxDate={end ?? undefined}
+        placeholder="start date"
+        includeTime={includeTime}
+      />
+      <Spacer spacing="12" />
+      <SingleDatePicker
+        value={end}
+        onChange={(d) => {
+          if (!d) return onChange([start, null]);
+
+          const newEnd = start && isAfter(start, d) ? addHours(start, 1) : d;
+          onChange([start, newEnd]);
+        }}
+        minDate={start ?? undefined}
+        placeholder="end date"
+        includeTime={includeTime}
+      />
+    </div>
   );
 };
 
-const StyledDateRangePicker = styled(DateRangePicker)`
-  .DayPicker-Day--today {
-    &,
-    &:hover {
-      color: hsl(352, 98%, 54%);
-    }
-  }
-  .DayPicker-Day--selected {
-    &,
-    &:hover {
-      color: white;
-    }
-  }
+interface SingleDatePickerProps {
+  value: Date | null;
+  onChange: (d: Date | null) => void;
+  placeholder: string;
+  includeTime: boolean;
+  minDate?: Date;
+  maxDate?: Date;
+}
+
+/**
+ * A single date and time picker
+ */
+const SingleDatePicker = (props: SingleDatePickerProps) => {
+  const { onChange, includeTime } = props;
+  return (
+    <SingleContainer>
+      <DateInput
+        {...props}
+        // common props
+        showActionsBar
+        canClearSelection
+        formatDate={(date) =>
+          format(date, includeTime ? "yyyy MMM dd HH:mm" : "yyyy MMM dd")
+        }
+        parseDate={(str) => new Date(str)}
+        timePrecision={includeTime ? TimePrecision.MINUTE : undefined}
+        dayPickerProps={{
+          firstDayOfWeek: 1,
+        }}
+      />
+      <Button
+        type="text"
+        icon={<CloseOutlined />}
+        onClick={() => onChange(null)}
+      />
+    </SingleContainer>
+  );
+};
+
+const SingleContainer = styled.div`
+  display: flex;
 `;
