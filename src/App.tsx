18d863c786fb3bad2994c496c67009f5803ffd13
williamsk91 <williamskusnandi@gmail.com>
2020-07-26 08:40:58 +1000

    feat: redirect / to /cal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "@fullcalendar/react";
 
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 import { CalendarProvider } from "./components/context/CalendarContext";
 import { Error } from "./components/flow/Error";
@@ -15,6 +15,7 @@ function App() {
   return (
     <CalendarProvider>
       <Switch>
+        <Redirect exact path="/" to={routes.home.index} />
         <Route exact path={routes.login} component={LoginRoute} />
         <PrivateRoute exact path={routes.setting} component={SettingRoute} />
         <PrivateRoute path={routes.home.index} component={HomeRoute} />
