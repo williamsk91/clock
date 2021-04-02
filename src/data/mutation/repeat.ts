5b3ac7cbfe9f7c5855528927a09e5642a1ecd152
williams <williamskusnandi@gmail.com>
2021-04-02 20:53:28 +1100

    feat: repeat cache update

--- src/data/mutation/repeat.ts
+++ src/data/mutation/repeat.ts
@@ -1,6 +1,8 @@
 import { useCallback } from "react";
 
 import {
+  Task,
+  TaskFragmentDoc,
   UpsertRepeatInput,
   useSetRepeatMutation,
 } from "../../graphql/generated";
@@ -25,6 +27,27 @@ export const useUpdateRepeat = () => {
                 exclude: upsertRepeatInput.exclude,
               }
             : null,
+        },
+        // task cache update
+        update: (cache, { data }) => {
+          // deletion
+          if (upsertRepeatInput === null) {
+            return cache.evict({ id: `Repeat:${data?.setRepeat.id}` });
+          }
+
+          const cachedTask = cache.readFragment<Task>({
+            id: `Task:${taskId}`,
+            fragment: TaskFragmentDoc,
+          });
+
+          cache.writeFragment({
+            id: `Task:${taskId}`,
+            fragment: TaskFragmentDoc,
+            data: {
+              ...cachedTask,
+              repeat: data?.setRepeat ?? undefined,
+            },
+          });
         },
       });
     },
