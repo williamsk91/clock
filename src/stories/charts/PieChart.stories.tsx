3124823a851c81accb04a0e6df0406c7523f88b6
williams <williamskusnandi@gmail.com>
2021-06-06 11:05:00 +1000

    feat: PieChart same size as WeekChart

--- src/stories/charts/PieChart.stories.tsx
+++ src/stories/charts/PieChart.stories.tsx
@@ -1,6 +1,9 @@
 import { PieChart, listsToPieData } from "../../components/charts/PieChart";
-import { getLists } from "../mocks";
+import { getLists, getRandomLists } from "../mocks";
 
 export default { title: "Charts / PieChart", component: PieChart };
 
 export const base = () => <PieChart data={listsToPieData(getLists())} />;
+
+const randomData = listsToPieData(getRandomLists(5));
+export const random = () => <PieChart data={randomData} />;
